38c82250c57afbaae5aa9d67e31b65f061eaba6f
Bruce Ediger <bediger@stratigery.com>
2018-02-24 09:35:09 -0700

    Add -g flag, and GraphViz dot-format output

--- src/expr/expr.go
+++ src/expr/expr.go
@@ -8,13 +8,25 @@ import (
 	"parser"
 )
 func main() {
+
+	graphVizOutput := false
+
 	str := os.Args[1]
+
+	if str == "-g" {
+		graphVizOutput = true
+		str = os.Args[2]
+	}
 
 	expr := bytes.NewBufferString(str+"\n")
 	lxr := lexer.NewFromFile(expr)
 	psr := parser.New(lxr)
 	xpr := psr.Parse()
 
-	fmt.Printf("%q\n", xpr)
+	if graphVizOutput {
+		xpr.GraphNode(os.Stdout)
+	} else {
+		fmt.Printf("%q\n", xpr)
+	}
 }
 
